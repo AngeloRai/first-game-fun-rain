62bfc1fb6484fbb90d401dfa713d20d226597321
Angelo Raimondi <angraimondi@gmail.com>
2021-04-06 12:37:25 -0300

    added voice and matched to the fruit images to make disapear

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+const pop = new Audio();
+pop.src = "./sounds/pop.wav";
+pop.volume = 0.1;
+
 class GameObject {
-  constructor(x, y, width, height, img, fruitX) {
+  constructor(x, y, width, height, img, fruitName) {
     this.x = x;
     this.y = y;
     this.width = width;
@@ -10,22 +14,24 @@ class GameObject {
     this.img = img;
     this.speedX = 0;
     this.speedY = 2;
-    this.fruitX = fruitX;
+    this.fruitName = fruitName;
   }
-  // updates the position of the fruits which are objects of GameObject class 
+  // updates the position of the fruits which are objects of GameObject class
   //called inside the update invoked inside the "update()"" method inside the Game class
   //(requestAnimationFrame)
   updatePosition() {
     this.x += this.speedX;
     this.y += this.speedY;
   }
-  // generate new images with updated position of the fruit objects invoked in the updateRainItems 
+  // generate new images with updated position of the fruit objects invoked in the updateRainItems
   //inside the forEach for every fruit
   draw() {
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
- 
 }
+
+
+
 
 
 
@@ -35,36 +41,64 @@ class Game {
     this.frames = 0;
     this.score = 0;
     this.animationId;
-    // address list of the images for the rain of fruits which is used to randomly generate new 
+    this.count = 0;
+    // address list of the images for the rain of fruits which is used to randomly generate new
     //fruits used as the source for the fruitImage.src =""
-    this.fruitList = [ 
-      {name: "strawberry", path: "./images/strawberry.png"}, 
-      {name: "apple", path: "./images/apple.png",},      
-      {name: "banana", path:"./images/banana.png"},
-      {name: "blackberry", path: "./images/blackberry.png"},
-      {name: "cherry", path: "./images/cherry.png"},
-      {name: "grape", path: "./images/grape.png"},
-      {name: "kiwi", path: "./images/kiwi.png"},
-      {name: "lemon", path: "./images/lemon.png"},
-      {name: "orange", path: "./images/orange.png"},
-      {name: "pear", path: "./images/pear.png"},
-      {name: "watermelon", path: "./images/watermelon.png"},
-      {name: "pineapple", path: "./images/pineapple.png"},
+    this.fruitList = [
+      { name: "strawberry", path: "./images/strawberry.png" },
+      { name: "apple", path: "./images/apple.png" },
+      { name: "banana", path: "./images/banana.png" },
+      { name: "blackberry", path: "./images/blackberry.png" },
+      { name: "cherry", path: "./images/cherry.png" },
+      { name: "grapes", path: "./images/grapes.png" },
+      { name: "kiwi", path: "./images/kiwi.png" },
+      { name: "lemon", path: "./images/lemon.png" },
+      { name: "orange", path: "./images/orange.png" },
+      { name: "pear", path: "./images/pear.png" },
+      { name: "watermelon", path: "./images/watermelon.png" },
+      { name: "pineapple", path: "./images/pineapple.png" },
+    ];
+    this.soundList = [
+      { name: "strawberry", path: "./sounds/strawberry.mp3" },
+      { name: "apple", path: "./sounds/apple.mp3" },
+      { name: "banana", path: "./sounds/banana.mp3" },
+      { name: "blackberry", path: "./sounds/blackberry.mp3" },
+      { name: "cherry", path: "./sounds/cherry.mp3" },
+      { name: "grapes", path: "./sounds/grapes.mp3" },
+      { name: "kiwi", path: "./sounds/kiwi.mp3" },
+      { name: "lemon", path: "./sounds/lemon.mp3" },
+      { name: "orange", path: "./sounds/orange.mp3" },
+      { name: "pear", path: "./sounds/pear.mp3" },
+      { name: "watermelon", path: "./sounds/watermelon.mp3" },
+      { name: "pineapple", path: "./sounds/pineapple.mp3" },
     ];
   }
-
+   
   removeFruit = (x, y) => {
-    this.fruits.forEach((fruit, index, current) => {
-      // console.log(`fruit x: ${fruit.x}, fruit y: ${fruit.x}`);
-      // console.log(`mouse x: ${x}, mouse y: ${x}`);
+    
+    const sounds = new Audio();
+    sounds.src = this.soundList[this.count].path;
+    let soundName = this.soundList[this.count].name;
+    sounds.play();
+    
+    this.fruits.forEach((fruit, index, array) => {
       if (
-        parseInt(x) >= parseInt(fruit.x)&&
-        parseInt(x) <= parseInt(fruit.x + 100)&&
-        parseInt(y) >= parseInt(fruit.y )&&
-        parseInt(y) <= parseInt(fruit.y + 100)
-        ) {
-        current.splice(index, 1);
-        console.log(fruit.fruitX);
+        parseInt(x) >= parseInt(fruit.x) &&
+        parseInt(x) <= parseInt(fruit.x + 80) &&
+        parseInt(y) >= parseInt(fruit.y) &&
+        parseInt(y) <= parseInt(fruit.y + 80)
+      ) {
+        if (soundName == fruit.fruitName) {
+          this.fruits.splice(index, 1);
+          console.log(`fruit name: ${fruit.fruitName}`);
+          console.log(`sound name: ${soundName}`);
+          this.count++;
+          sounds.play();
+          console.log(this.count);
+        }
+
+        // console.log(`fruit x: ${fruit.x}, fruit y: ${fruit.x}`);
+        // console.log(`mouse x: ${x}, mouse y: ${x}`);
       }
     });
   };
@@ -76,31 +110,44 @@ class Game {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     const bgImg = new Image();
-    bgImg.src = "/fun-rain/images/background-fruits.png";
-    const background = new GameObject(-50, -240, 1000, 320, bgImg);
+    bgImg.src = "/fun-rain/images/fruits-background-removebg-preview.png";
+    const background = new GameObject(0, -20, 900, 580, bgImg);
     background.draw();
 
     const bottomBgImg = new Image();
     bottomBgImg.src = "/fun-rain/images/background-flowers.png";
-    const bottomBackground = new GameObject(0, 400, 900, 350, bottomBgImg);
+    const bottomBackground = new GameObject(0, 340, 900, 450, bottomBgImg);
     bottomBackground.draw();
+
+    let randomFruit = Math.floor(Math.random() * this.fruitList.length);
+    const fruitImage = new Image();
+    fruitImage.src = this.fruitList[randomFruit].path;
 
     this.fruits.forEach((fruit) => {
       fruit.updatePosition();
       fruit.draw();
     });
 
-    const fruitImage = new Image();
-    let randomFruit = Math.floor(Math.random() * this.fruitList.length);
+    const sound = new Audio();
+    sound.src = this.soundList[this.count].path;
 
-    fruitImage.src = this.fruitList[randomFruit].path;
+    if (this.frames % 240 === 0) {
+      sound.play()
+    }
 
-    if (this.frames % 30 === 0) {
-      const originY = 10;
+    if (this.frames % 15 === 0) {
+      const originY = 0;
 
       const maxX = canvas.width;
       const randomX = Math.floor(Math.random() * (maxX - 20));
-      const fruit = new GameObject(randomX, originY, 80, 80, fruitImage, this.fruitList[randomFruit].name);
+      const fruit = new GameObject(
+        randomX,
+        originY,
+        80,
+        80,
+        fruitImage,
+        this.fruitList[randomFruit].name
+      );
 
       this.fruits.push(fruit);
       this.score += 5;
@@ -112,7 +159,6 @@ class Game {
   };
 
   updateGame = () => {
-    
     // this.removeFruit(this.fruit.x, this.fruit.y)
     this.clear();
     // this.updateScore();
@@ -132,7 +178,7 @@ window.onload = () => {
     const rect = canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
-    // console.log("x: " + x + " y: " + y)
+    // console.log("x: " + x + " y: " + y);
     // console.log(rect);
     game.removeFruit(x, y);
   }
@@ -140,5 +186,7 @@ window.onload = () => {
   const canvas = document.querySelector("canvas");
   canvas.addEventListener("click", function (e) {
     removeFruitCursorPosition(canvas, e);
+    pop.play();
+    
   });
 };
